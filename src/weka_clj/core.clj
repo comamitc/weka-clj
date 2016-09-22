8c5b5cc880df814c2f01f8219f3d9911939201cb
Mitch Comardo <comamitc@gmail.com>
2016-09-22 12:47:31 -0600

    transform attributes and data into a list of instances

--- src/weka_clj/core.clj
+++ src/weka_clj/core.clj
@@ -1,24 +1,51 @@
 (ns weka-clj.core
   (:require [clojure.core.reducers :as r])
-  (:import [weka.core Attribute]))
+  (:import [weka.core Attribute DenseInstance Instances]))
 
-(defn- ->attrs [acc k v]
-  (let [attr (if (every? number? v)
-               (Attribute. (name k))
-               (Attribute. (name k) v))]
-    (assoc acc k attr)))
+(defn- ->attrs
+  ([] {})
+  ([xs k v]
+   (let [attr (if (every? number? v)
+                (Attribute. (name k))
+                (Attribute. (name k) v))]
+     (assoc xs k attr))))
 
 (defn- collect-raw [acc k v]
   (update-in acc [k] conj v))
 
-(defn- ->raw [acc curr]
-  (r/reduce collect-raw acc curr))
+;; @TODO: implement fold here
+(defn- ->raw
+  ([] {})
+  ([xs x]
+   (r/reduce collect-raw xs x)))
 
-(defn reduce-attributes [attrs]
-  (let [comp-fn (comp (partial r/reduce ->attrs {})
-                      (partial r/reduce ->raw {}))]
-    (comp-fn attrs)))
+(defn reduce-attributes [data]
+  (let [comp-fn (comp (partial r/fold ->attrs)
+                      (partial r/fold ->raw))]
+    (comp-fn data)))
+
+(defn- ->instances [attrs capacity]
+    (let [dataset (Instances. "users"
+                              (java.util.ArrayList. (vals attrs))
+                              capacity)]
+      (fn
+        ([] (list))
+        ([xs x]
+         (let [num-attrs (count (keys attrs))
+               instance  (DenseInstance. num-attrs)]
+           (.setDataset instance dataset)
+           (loop [kws (keys x)
+                  cnt 0]
+             (let [k (first kws)]
+               (if (nil? k)
+                 ;; @TODO: should this return the Instances DataSet?
+                 (conj xs instance)
+                 ;; else
+                 (let [a (get x k)
+                       v (if (number? a) (double a) a)]
+                   (.setValue instance cnt v)
+                   (recur (rest kws) (+ 1 cnt)))))))))))
 
 (defn maps->instances [list]
   (let [attrs (reduce-attributes list)]
-    nil))
+    (r/fold (->instances attrs (count list)) list)))
--- test/runner.clj
+++ test/runner.clj
@@ -1,12 +1,12 @@
 (ns runner
   (:require [clojure.test :refer :all]
-            [clojure.core.reducers :as r]
             [weka-clj.core :refer [maps->instances reduce-attributes]])
   (:import [weka.core Instance Attribute]))
 
-; (deftest test-list
-;   (let [instances (maps->instances [{:foo "bar" :baz 1} {:foo "boo" :baz 2}])]
-;     (is (instance? Instance (first instances)))))
+(deftest test-list
+  (let [instances (maps->instances [{:foo "bar" :baz 1} {:foo "boo" :baz 2} {:foo "bob" :baz 3}])]
+    (is (every? #(instance? Instance %) instances))
+    (is (every? #(= 2 (.numValues %)) instances))))
 
 (deftest test-attribute-creation
   (let [attrs (reduce-attributes [{:foo "bar" :baz 1} {:foo "boo" :baz 2}])]
