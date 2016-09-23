5d861582af7c4e5c2d13956e8200b4423ea1cd8c
Mitch Comardo <comamitc@gmail.com>
2016-09-23 14:48:56 -0600

    reorganize

---
+++ src/weka_clj/feature/extraction.clj
@@ -0,0 +1,54 @@
+(ns weka-clj.feature.extraction
+  (:require [clojure.core.reducers :as r])
+  (:import [weka.core Attribute DenseInstance Instances]))
+
+(defn- ->attrs
+  ([] {})
+  ([xs k v]
+   (let [attr (if (every? number? v)
+                (Attribute. (name k))
+                (Attribute. (name k) v))]
+     (assoc xs k attr))))
+
+(defn- collect-raw [acc k v]
+  (update-in acc [k] conj v))
+
+(defn- ->raw
+  ([] {})
+  ([xs x]
+   (r/reduce collect-raw xs x)))
+
+(defn- ->instances [attrs capacity]
+  (fn
+    ([] (Instances. "users"
+                    (java.util.ArrayList. (vals attrs))
+                    capacity))
+    ([xs x]
+     (let [num-attrs (count (keys attrs))
+           instance  (DenseInstance. num-attrs)]
+       (.setDataset instance xs)
+       (loop [kws (keys x)
+              cnt 0]
+         (let [k (first kws)]
+           (if (nil? k)
+             ;; then
+             xs
+             ;; else
+             (let [a (get x k)
+                   v (if (number? a) (double a) a)]
+               (.setValue instance cnt v)
+               (recur (rest kws) (+ 1 cnt))))))))))
+
+;; @TODO: implement fold here
+(defn attributize
+  "Takes a sequence of maps and returns a `list` of `weka.core.Attributes`."
+  [data]
+  (let [comp-fn (comp (partial r/fold ->attrs)
+                      (partial r/fold ->raw))]
+    (comp-fn data)))
+
+(defn maps->instances
+  "Takes a sequence of maps and returns `weka.core.Instances`."
+  [list]
+  (let [attrs (attributize list)]
+    (r/fold (->instances attrs (count list)) list)))
--- test/runner.clj
+++ test/runner.clj
@@ -1,16 +1,16 @@
 (ns runner
   (:require [clojure.test :refer :all]
-            [weka-clj.core :refer [maps->instances reduce-attributes]])
+            [weka-clj.feature.extraction :refer [maps->instances attributize]])
   (:import [weka.core Instance Instances Attribute]))
 
-(deftest test-list
+(deftest test-map->instances
   (let [instances (maps->instances [{:foo "bar" :baz 1} {:foo "boo" :baz 2} {:foo "bob" :baz 3}])]
     (is (instance? Instances instances))
     (is (every? #(instance? Instance %) instances))
     (is (every? #(= 2 (.numValues %)) instances))))
 
 (deftest test-attribute-creation
-  (let [attrs (reduce-attributes [{:foo "bar" :baz 1} {:foo "boo" :baz 2}])]
+  (let [attrs (attributize [{:foo "bar" :baz 1} {:foo "boo" :baz 2}])]
     (is (= (count (keys attrs)) 2))
     (is (instance? Attribute (:foo attrs)))
     (is (= true (.isNumeric (:baz attrs))))
