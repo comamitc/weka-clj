6566eefc4bfbb1599f343ef7921eca938ae8be94
Mitch Comardo <comamitc@gmail.com>
2016-09-27 10:27:39 -0600

    increment version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject weka-clj "0.1.3"
+(defproject weka-clj "0.1.4"
   :description "Tools for weka written in Clojure"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"
